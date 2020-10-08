cba6f80c9f4aacde80acc203d785b4b3912d4471
Nastya <bagaeva00@yandex.ru>
2020-10-08 13:01:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function p1() {
+  let b = document.getElementById("hr1");
+  b.align = "left";
+}
+function p2() {
+  let b = document.getElementById("hr1");
+  b.align = "center";
+}
+function p3() {
+  let b = document.getElementById("hr1");
+  b.align = "right";
+}
